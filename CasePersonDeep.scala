2bfd8cd440e1607ca5b9ef1e8c902f3cee61baa8
praful <praful@thoughtworks.com>
2012-06-18 18:47:38 -0400

    removed unnecessary files; renamed some to match feature

--- CasePersonDeep.scala
+++ CasePersonDeep.scala
@@ -5,7 +5,4 @@ var pep = new Person("Pep", "Barcelona", 50, null)
 val messi = new Person("Lionel", "Barcelona", 24, pep)
 
 val messiCopy = messi.copy(city = "Rosario")
-pep = null
-println(messiCopy.manager) // Person(Lionel,Rosario,24)
-println(pep) // null
-
+println(messi.manager equals messiCopy.manager)
